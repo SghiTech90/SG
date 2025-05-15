05d5793e87cf3ba15e9b745f341f054303a04daa
Rajwardhan Patil <rajpatil2k@gmail.com>
2025-05-15 18:42:05 +0530

    sd

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const app = express();
 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' })); // or higher if needed
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 
 
